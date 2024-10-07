eb0eb06ab432e34344c714ec94419090d3b771c8
lukby2457 <kjh7442455@gmail.com>
2024-10-07 13:53:08 +0900

    update: route.ts 잘못된 try 삭제 및 에러 핸들링 제거

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -3,29 +3,21 @@ import { ChampionRotation } from "@/types/ChampionRotation";
 const RIOT_API_KEY: string | undefined = process.env.NEXT_PUBLIC_RIOT_API_KEY;
 
 export async function GET() {
-  // try {
-    const res = await fetch(
-      "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
-      {
-        headers: {
-          "X-Riot-Token": `${RIOT_API_KEY}`,
-        },
-      }
-    );
-
-    if (!res.ok) {
-      throw new Error(`Error: ${res.status} ${res.statusText}`);
+  const res = await fetch(
+    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
+    {
+      headers: {
+        "X-Riot-Token": `${RIOT_API_KEY}`,
+      },
     }
+  );
 
-    const data: ChampionRotation = await res.json();
-    const newData = data.freeChampionIds;
-
-    return Response.json({ data: newData });
-  // } catch (error) {
-  //   console.error("Error fetching champion rotations:", error);
-  //   return Response.json(
-  //     { message: "Failed to fetch champion rotations" },
-  //     { status: 500 }
-  //   );
+  // if (!res.ok) {
+  //   throw new Error(`Error: ${res.status} ${res.statusText}`);
   // }
+
+  const data: ChampionRotation = await res.json();
+  const newData = data.freeChampionIds;
+
+  return Response.json({ data: newData });
 }
